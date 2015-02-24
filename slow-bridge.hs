d68aba5450c8f16e9e98dc5184eb5492cf6d3b6e
Kai Harries <kai.harries@gmail.com>
2015-02-24 21:53:13 +0100

    Add functionality to webapp

--- slow-bridge.hs
+++ slow-bridge.hs
@@ -3,27 +3,41 @@
 module Main where
 
 import Control.Applicative ((<$>), optional)
-import Control.Monad (msum)
+import Control.Monad (msum, void, when)
+import Control.Monad.IO.Class (liftIO)
+import Data.Char (isDigit)
+import Data.List (find, isInfixOf, isPrefixOf)
 import Data.Maybe (fromMaybe)
 import Data.Text (Text)
 import Data.Text.Lazy (unpack)
 import Happstack.Server hiding (body)
-import Text.Blaze.Html5 (Html, (!), a, form, input, p, toHtml, label)
-import Text.Blaze.Html5.Attributes (action, enctype, href, name, size, type_, value)
+import System.Process (readProcess)
+import Text.Blaze.Html5 (Html, (!), form, input, toHtml, toValue, label)
+import Text.Blaze.Html5.Attributes (action, enctype, name, type_, value)
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
 
 main :: IO ()
-main = simpleHTTP nullConf
-       $  do decodeBody (defaultBodyPolicy "/tmp/" 4096 4096 4096)
-             handlers
+main = do
+       putStrLn "Open http://localhost:8000/netem in a browser"
+       simpleHTTP nullConf $ do
+           decodeBody (defaultBodyPolicy "/tmp/" 4096 4096 4096)
+           handlers
 
 
 handlers :: ServerPart Response
-handlers = msum
-  [ dir "netem"   $ netem
-  , netem
-  ]
+handlers = do
+  nics <- liftIO getNICs
+  msum [ dir "netem" $ netem nics
+       , seeOther ("/netem" :: String) $ toResponse ()
+       ]
+  where
+    getNICs :: IO [String]
+    getNICs = do
+        ls <- readProcess "/bin/ip" ["link"] ""
+        let nics = [strip x | x <- lines ls, isDigit $ head x]
+        return [x | x <- nics, x /= "lo" && take 3 x /= "vir"]
+        where strip = takeWhile (/= ':') . drop 2 . dropWhile (/= ':')
 
 
 template :: Text -> Html -> Response
@@ -35,55 +49,100 @@ template title body = toResponse $
       body
 
 
-netem :: ServerPart Response
-netem = msum [ viewNetem, updateNetem ]
+netem :: [String] -> ServerPart Response
+netem ns = msum [ viewNetem, updateNetem ]
     where
       viewNetem :: ServerPart Response
       viewNetem =
           do method GET
-             rate1  <- lookCookieValue' "512kbit" "rate1"
-             rate2  <- lookCookieValue' "512kbit" "rate2"
-             delay1 <- lookCookieValue' "10ms"    "delay1"
-             delay2 <- lookCookieValue' "10ms"    "delay2"
+             nic1   <- optional $ lookCookieValue "nic1"
+             nic2   <- optional $ lookCookieValue "nic2"
+             rate1  <- optional $ lookCookieValue "rate1"
+             rate2  <- optional $ lookCookieValue "rate2"
+             delay1 <- optional $ lookCookieValue "delay1"
+             delay2 <- optional $ lookCookieValue "delay2"
              ok $ template "Network Emulator" $ do
-                    H.h1 "Network Emulator"
-                    H.p "Would set network emulation to:"
-                    H.p $ toHtml $ rate1 ++ " " ++ delay1 ++ " / " ++ rate2 ++ " " ++ delay2
-                    form ! action "/netem" ! enctype "multipart/form-data" ! A.method "POST" $ do
-                    H.p $ do label ! A.for "rate1" $ "From eth0 to eth1: "
-                             H.select ! A.id "rate1" ! name "new_rate1" ! ralign $ rates
-                             label ! A.for "delay1" $ " Delay: "
-                             H.select ! A.id "delay1" ! name "new_delay1" ! ralign $ delays
-                    H.p $ do label ! A.for "rate2" $ "From eth1 to eth0: "
-                             H.select ! A.id "rate1" ! name "new_rate2" ! ralign $ rates
-                             label ! A.for "delay2" $ " Delay: "
-                             H.select ! A.id "delay2" ! name "new_delay2" ! ralign $ delays
-                    H.p $ input ! type_ "submit" ! value "Apply"
+                H.h1 "Network Emulator"
+                form ! action "/netem" ! enctype "multipart/form-data" ! A.method "POST" $ do
+                H.p $ do label ! A.for "nics" $ "Select the network interface cards to bridge: "
+                         H.select ! name "new_nic1" $ nics nic1
+                         label " <-> "
+                         H.select ! name "new_nic2" $ nics nic2
+                H.p $ do label ! A.for "rate2" $ fromTo nic1 nic2
+                         H.select ! name "new_rate2"  ! ralign $ rates rate2
+                         label " Delay: "
+                         H.select ! name "new_delay2" ! ralign $ delays delay2
+                H.p $ do label ! A.for "rate1" $ fromTo nic2 nic1
+                         H.select ! name "new_rate1"  ! ralign $ rates rate1
+                         label " Delay: "
+                         H.select ! name "new_delay1" ! ralign $ delays delay1
+                H.p $ input ! type_ "submit" ! value "Apply"
         where
-          rates = do H.option   "32kbit" ! value   "32kbit"
-                     H.option   "64kbit" ! value   "64kbit"
-                     H.option  "128kbit" ! value  "128kbit"
-                     H.option  "256kbit" ! value  "256kbit"
-                     H.option  "512kbit" ! value  "512kbit"
-                     H.option "1024kbit" ! value "1024kbit"
-          delays = do H.option   "10ms"  ! value   "10ms"
-                      H.option   "40ms"  ! value   "40ms"
-                      H.option  "160ms"  ! value  "160ms"
-                      H.option  "320ms"  ! value  "320ms"
-                      H.option  "640ms"  ! value  "640ms"
+          rates def  = do toOpt def   "32kbit"
+                          toOpt def   "64kbit"
+                          toOpt def  "128kbit"
+                          toOpt def  "256kbit"
+                          toOpt def  "512kbit"
+                          toOpt def "1024kbit"
+          delays def = do toOpt def  "10ms"
+                          toOpt def  "40ms"
+                          toOpt def "160ms"
+                          toOpt def "320ms"
+                          toOpt def "640ms"
+          nics def = mapM_ (toOpt def) ns
           ralign = A.style "text-align:right"
-          lookCookieValue' d b = fromMaybe d <$> (optional $ lookCookieValue b)
+          toOpt :: Maybe String -> String -> Html
+          toOpt def v = (if (Just v == def) then (! A.selected "") else id)
+                        $ H.option (toHtml v) ! value (toValue v)
+          fromTo a b = do "From "
+                          H.b $ toHtml $ fromMaybe "A" a
+                          " to "
+                          H.b $ toHtml $ fromMaybe "B" b
+                          ": "
 
       updateNetem :: ServerPart Response
       updateNetem =
           do method POST
-             rate1  <- lookText "new_rate1"
-             rate2  <- lookText "new_rate2"
-             delay1 <- lookText "new_delay1"
-             delay2 <- lookText "new_delay2"
-             addCookies [ (Session, mkCookie "rate1"  (unpack rate1))
-                        , (Session, mkCookie "rate2"  (unpack rate2))
-                        , (Session, mkCookie "delay1" (unpack delay1))
-                        , (Session, mkCookie "delay2" (unpack delay2))
+             nic1   <- unpack <$> lookText "new_nic1"
+             nic2   <- unpack <$> lookText "new_nic2"
+             rate1  <- unpack <$> lookText "new_rate1"
+             rate2  <- unpack <$> lookText "new_rate2"
+             delay1 <- unpack <$> lookText "new_delay1"
+             delay2 <- unpack <$> lookText "new_delay2"
+             liftIO $ updateBridge nic1 nic2
+             liftIO $ tc nic1 delay1 rate1
+             liftIO $ tc nic2 delay2 rate2
+             addCookies [ (Session, mkCookie "nic1"   nic1)
+                        , (Session, mkCookie "nic2"   nic2)
+                        , (Session, mkCookie "rate1"  rate1)
+                        , (Session, mkCookie "rate2"  rate2)
+                        , (Session, mkCookie "delay1" delay1)
+                        , (Session, mkCookie "delay2" delay2)
                         ]
              seeOther ("/netem" :: String) (toResponse ())
+          where
+            ipLink a = void $ readProcess "/bin/ip" ("link":a) ""
+            tc n d r = tc' ["qdisc", "replace", "dev", n, "root", "netem"
+                           , "delay", d, "rate", r]
+              where tc' a = void $ readProcess "/sbin/tc" a ""
+            updateBridge nic1 nic2 = do
+              b <- find ("brSlow" `isPrefixOf`) <$> lines <$> brctl ["show"]
+              when (isDeleteNeeded b) deleteBridge
+              when (isUpdateNeeded b) updateBridge'
+              where
+                isUpdateNeeded Nothing  = True
+                isUpdateNeeded (Just b) | not (nic1 `isInfixOf` b) = True
+                                        | not (nic2 `isInfixOf` b) = True
+                                        | otherwise                = False
+                isDeleteNeeded Nothing  = False
+                isDeleteNeeded b        = isUpdateNeeded b
+                deleteBridge  = do ipLink ["set", "down", "dev", "brSlow"]
+                                   void $ brctl ["delbr", "brSlow"]
+                updateBridge' = do void $ brctl ["addbr", "brSlow"]
+                                   void $ brctl ["setfd", "brSlow", "0"]
+                                   void $ brctl ["addif", "brSlow", nic1]
+                                   void $ brctl ["addif", "brSlow", nic2]
+                                   ipLink ["set", "up", "dev", nic1]
+                                   ipLink ["set", "up", "dev", nic2]
+                                   ipLink ["set", "up", "dev", "brSlow"]
+                brctl a = readProcess "/sbin/brctl" a ""
